b3d6747692749caed2b8479462f5d39f3a1a0258
Rohit Kumar Mahato <95642350+Rohitkr2510@users.noreply.github.com>
2024-04-12 19:47:28 +0000

    let const var

---
+++ basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 14454
+let accountEmaild = "rohit@gamil.com"
+var accountPassword = "12345"
+accountCity = "Dhanbad"
+let accountState;
+
+//accountId = 2 // not allowed
+
+accountEmail = "rk@gamil.com"
+accountPassword = "2323232"
+accountCity = "Dhanbad"
+
+console.log(accountEmaild);
+console.table([accountId, accountEmaild,accountPassword, accountCity, accountState])
